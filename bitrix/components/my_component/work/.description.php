2566ba057cfeb75db60418bfeed9df194c3f1458
IsayevFazil <fazil.isayev.1992@mail.ru>
2020-04-09 15:04:28 +0300

    calendar

---
+++ bitrix/components/my_component/work/.description.php
@@ -0,0 +1,15 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentDescription = array(
+	"NAME" => GetMessage("PRV_VOTE_TITLE"),
+	"DESCRIPTION" => GetMessage("PRV_VOTE_DESC"),
+	"ICON" => "",	
+	"PATH" => array(
+		"ID" => "prvolga",		
+		"NAME" => GetMessage("PRV_NAME")
+		
+	),
+);
+
+?>
---
+++ bitrix/components/my_component/work/lang/ru/.description.php
@@ -0,0 +1,4 @@
+<?
+$MESS['PRV_VOTE_TITLE'] = "Рабочие  дни";
+$MESS['PRV_VOTE_DESC'] = "Компонент рабочих дней с привязкой к инфоблоку";
+$MESS['PRV_NAME'] = "My component";
